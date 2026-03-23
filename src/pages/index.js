8d44ccdbc5dfa6449e3b3b94cdd6aa8a8a138fa0
ethungshanshitiri <47774202+ethungshanshitiri@users.noreply.github.com>
2026-03-23 22:05:47 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
         <div className="container">
           <div className={styles.heroGrid}>
             <div>
-              <p className={styles.eyebrow}>BeNiFIt</p>
+              
               <h1 className={styles.title}>
                 Towards real-time in vivo biomarker sensing through intrabody nanoscale communication
               </h1>
